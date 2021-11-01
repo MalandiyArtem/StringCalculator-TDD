82407c583540fbc60391636e1a6673e3c81ea1cd
MalandiyArtemWork <malandiyartem@gmail.com>
2021-11-01 11:40:52 +0200

    Added new class Test2. Added test method where method Add handle an unknown amount of numbers

---
+++ StringCalculatorTests/Test2.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using StringCalculatorLibrary;
+
+namespace StringCalculatorTests
+{
+    class Test2
+    {
+        StringCalculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new StringCalculator();
+        }
+
+        [Test]
+        public void Add_handle_an_unknown_amount_of_numbers()
+        {
+            string input = "1,2,3,4,5,6,7,8,9";
+            int expected = 45;
+
+            int actual = calc.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
