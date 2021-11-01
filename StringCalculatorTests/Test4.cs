c7e9b6c7fce703fc78a91bcad7c41f8af46f9b84
MalandiyArtemWork <malandiyartem@gmail.com>
2021-11-01 13:06:41 +0200

    Added new class Test4. Added test method where method Add supports different delimeters

---
+++ StringCalculatorTests/Test4.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using StringCalculatorLibrary;
+
+namespace StringCalculatorTests
+{
+    class Test4
+    {
+        StringCalculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new StringCalculator();
+        }
+
+        [Test]
+        public void Add_supports_different_delimeters()
+        {
+            string input1 = "//;\n1;2;3";
+            string input2 = "//:\n1:2:3";
+            string input3 = "//-\n1-2-3";
+            int expected = 6;
+
+            int actual1 = calc.Add(input1);
+            int actual2 = calc.Add(input2);
+            int actual3 = calc.Add(input3);
+
+            Assert.AreEqual(expected, actual1);
+            Assert.AreEqual(expected, actual2);
+            Assert.AreEqual(expected, actual3); 
+        }
+    }
+}
