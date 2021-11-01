74f08b043ac84669c15d5729fbba3df47ee38e5f
MalandiyArtemWork <malandiyartem@gmail.com>
2021-11-01 11:57:31 +0200

    Added new class Test3. Added test method where method Add handle new lines and comma as delimiter

---
+++ StringCalculatorTests/Test3.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using StringCalculatorLibrary;
+
+namespace StringCalculatorTests
+{
+    class Test3
+    {
+        StringCalculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new StringCalculator();
+        }
+
+        [Test]
+        public void Add_handle_new_lines_and_comma_as_delimiter()
+        {
+            string input = "1,2,3\n4\n5,6,7\n8,9";
+            int expected = 45;
+
+            int actual = calc.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
