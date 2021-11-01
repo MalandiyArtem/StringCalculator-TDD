0e14e89e851882cd24a9802170faaafc2db12b66
MalandiyArtemWork <malandiyartem@gmail.com>
2021-11-01 17:04:26 +0200

    Added class for custom exception. Added test for negative numbers

---
+++ StringCalculatorLibrary/NegativesNotAllowedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculatorLibrary
+{
+    public class NegativesNotAllowedException : Exception
+    {
+        public NegativesNotAllowedException(string message)
+            : base(message) { }
+
+        public NegativesNotAllowedException() { }
+    }
+}
---
+++ StringCalculatorTests/Test5.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using StringCalculatorLibrary;
+using System;
+
+namespace StringCalculatorTests
+{
+    class Test5
+    {
+        StringCalculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new StringCalculator();
+        }
+
+        [Test]
+        public void Add_negative_multiply_numbers_throws_exception()
+        {
+            string input1 = "1,2,-3\n4\n-5,6,-7\n8,9";
+
+            var ex = Assert.Throws<NegativesNotAllowedException>(() => calc.Add(input1));
+            Assert.That(ex.Message, Is.EqualTo("-3 -5 -7"));
+        }
+
+        [Test]
+        public void Add_negative_number_throws_exception()
+        {
+            string input = "1,2,3\n4\n5,6,-7\n8,9";
+            Assert.Throws<NegativesNotAllowedException>(() => calc.Add(input));
+        }
+    }
+}
