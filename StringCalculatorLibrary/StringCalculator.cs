2361cff67daeff87af0770cb81326f31574f7a53
MalandiyArtemWork <malandiyartem@gmail.com>
2021-11-01 11:26:22 +0200

    Method Add was refactoried

--- StringCalculatorLibrary/StringCalculator.cs
+++ StringCalculatorLibrary/StringCalculator.cs
@@ -6,16 +6,15 @@ namespace StringCalculatorLibrary
     {
         public int Add(string numbers)
         {
+            string[] nums = numbers.Split(',');
+
             if (numbers.Trim() == "")
                 return 0;
-            if (numbers.Length == 1)
-                return int.Parse(numbers);
-            if(numbers.Length == 3)
-            {
-                string[] nums = numbers.Split(',');
+            if (nums.Length == 1)
+                return int.Parse(nums[0]);
+            if (nums.Length == 2)
                 return int.Parse(nums[0]) + int.Parse(nums[1]);
-            }
-                
+
             return -1;
         }
     }
