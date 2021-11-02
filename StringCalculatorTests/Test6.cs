d1337b179712f9d76404acf8ccc740a76ba6f4e7
MalandiyArtemWork <malandiyartem@gmail.com>
2021-11-02 09:55:59 +0200

    Added test for numbers over 1000. They will be ignored during sum

---
+++ StringCalculatorTests/Test6.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using StringCalculatorLibrary; 
+
+namespace StringCalculatorTests
+{
+    class Test6
+    {
+        StringCalculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new StringCalculator();
+        }
+
+        [Test]
+        public void Add_number_more_than_1000_ignored()
+        {
+            string input = "1,2,3\n4\n5,6,1004,1343,7\n8,9";
+            int expected = 45;
+
+            int actual = calc.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
